3aa3eda427bc496147d7e8e8871c3311e52b776c
Moon-Muzic <system677155@gmail.com>
2025-12-20 10:45:10 +0530

    Moon

--- MoonMuzic/utils/stream/queue.py
+++ MoonMuzic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from EsproMusic.misc import db
-from EsproMusic.utils.formatters import check_duration, seconds_to_min
+from MoonMuzic.misc import db
+from MoonMuzic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
