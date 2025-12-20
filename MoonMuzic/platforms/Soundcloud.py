5edbd41d988c5911d7ee49b3a1e90ada095b98c9
Moon-Muzic <system677155@gmail.com>
2025-12-20 10:31:33 +0530

    Moon

--- MoonMuzic/platforms/Soundcloud.py
+++ MoonMuzic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from EsproMusic.utils.formatters import seconds_to_min
+from MoonMuzic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
