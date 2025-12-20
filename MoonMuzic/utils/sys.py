887529e4b67b42a1b3b88cebcf0d38f0deb2a14e
Moon-Muzic <system677155@gmail.com>
2025-12-20 10:38:52 +0530

    Moon

--- MoonMuzic/utils/sys.py
+++ MoonMuzic/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from EsproMusic.misc import _boot_
-from EsproMusic.utils.formatters import get_readable_time
+from MoonMuzic.misc import _boot_
+from MoonMuzic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
