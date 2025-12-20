08ed20aecc354c906bb5bb34739f9fddce42a5b5
Moon-Muzic <system677155@gmail.com>
2025-12-20 10:42:41 +0530

    Moon

--- MoonMuzic/utils/inline/admins.py
+++ MoonMuzic/utils/inline/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from EsproMusic import app
-from EsproMusic.misc import SUDOERS, db
-from EsproMusic.utils.database import (
+from MoonMuzic import app
+from MoonMuzic.misc import SUDOERS, db
+from MoonMuzic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
